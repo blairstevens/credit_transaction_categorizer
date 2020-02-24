7ae2545ed4ed019f89a82d3321166bf85b012c58
Blair Stevens <stevens.blair.e@gmail.com>
2020-02-24 15:08:28 -0500

    reorder of functions and better commenting within sandbox"

--- convert-sandbox.py
+++ convert-sandbox.py
@@ -7,11 +7,22 @@ from gazpacho import Soup
 from sklearn.preprocessing import LabelBinarizer
 from fire import Fire
 
+# Aggregate html files in data folder
+def find_html_files():
+    html_files = []
+    for file in listdir('data/'):
+        if isfile(join('data/', file)):
+            if file.endswith(".html"):
+                html_files.append(file)
+    return html_files
+
+# Soup the html file to be able to work on it
 def souper_finder(name):
     soup = Soup((open(f"data/{name}").read()), "html.parser")
     found = soup.find_all(class_= 'transaction-row')
     return found
 
+# Pull the date from the transaction and build it into a list
 def date_ripper(found):
     dates = []
     for i in found:
@@ -21,6 +32,7 @@ def date_ripper(found):
         dates.append(x)
     return dates
 
+# Pull the description from the transaction and build it into a list
 def desc_ripper(found):
     desc = []
     for i in found:
@@ -28,6 +40,7 @@ def desc_ripper(found):
         desc.append(y)
     return desc
 
+# Pull the value from the transaction and build it into a list
 def val_ripper(found):
     val = []
     for i in found:
@@ -35,6 +48,7 @@ def val_ripper(found):
         val.append(z)
     return val
 
+# Pull the image string from the transaction and build it into a list
 def img_ripper(found):
     img = []
     for i in found:
@@ -43,6 +57,7 @@ def img_ripper(found):
         img.append(a)
     return img
 
+# Build a dataframe from the lists
 def frame_builder(dates, desc, val, img):
     tog = list(zip(dates, desc, val, img))
     df = pd.DataFrame(tog, columns=['Date', "Desc", "Value", "Category"])
@@ -50,14 +65,7 @@ def frame_builder(dates, desc, val, img):
     df.Date = pd.to_datetime(df.Date)
     return df
 
-def find_html_files():
-    html_files = []
-    for file in listdir('data/'):
-        if isfile(join('data/', file)):
-            if file.endswith(".html"):
-                html_files.append(file)
-    return html_files
-
+# Stack the previous functions and return a dataframe
 def html_to_frames():
     html_files = find_html_files()
     df = pd.DataFrame(columns=['Date', "Desc", "Value", "Category"])
@@ -71,6 +79,7 @@ def html_to_frames():
         df = df.append(xf, ignore_index=True)
     return df
 
+# Cleanup date and binarize the category
 def frame_cleaner(df):
     # df.Value = df['Value'].str.extract("(\d*\.?\d+)").astype(float)
     df.Date = pd.to_datetime(df.Date)
@@ -80,15 +89,18 @@ def frame_cleaner(df):
                          index=df.index))
     return df
 
+# Export dataframe to excel file, using date as title
 def xport_frame(df):
     timestr = time.strftime("%Y%m%d")
     excel = df.to_excel(f"output/{timestr}.xlsx")
 
+# One function to rule them all.
 def convert():
     frame = html_to_frames()
     df = frame_cleaner(frame)
     xport_frame(df)
 
+# Use Fire to make it a CLI
 if __name__ == '__main__':
     Fire({
         'convert': convert,
